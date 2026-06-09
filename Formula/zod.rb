4b18cc1ef4403980a80a0dbef3449a83fc8022d7
Bartosz Fenski <fenio@debian.org>
2026-06-09 17:48:14 +0200

    Add zod formula (v1.0.0)

---
+++ Formula/zod.rb
@@ -0,0 +1,24 @@
+class Zod < Formula
+  desc "Modernized SDL2 build of the Zod Engine (remake of the 1996 RTS 'Z')"
+  homepage "https://github.com/fenio/zod"
+  url "https://github.com/fenio/zod/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "30da9a1a4856e92912f5a4c0e6b837d637c655bee6aa208e718dd5af9a972e09"
+  license "GPL-3.0-or-later"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "sdl2"
+  depends_on "sdl2_image"
+  depends_on "sdl2_mixer"
+  depends_on "sdl2_ttf"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_path_exists bin/"zod"
+  end
+end
