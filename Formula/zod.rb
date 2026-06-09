12580c3184b1ace121a1b6c6ed69c870b42be640
Bartosz Fenski <fenio@debian.org>
2026-06-10 00:15:25 +0200

    zod 0.2.3

--- Formula/zod.rb
+++ Formula/zod.rb
@@ -1,14 +1,10 @@
 class Zod < Formula
   desc "Modernized SDL2 build of the Zod Engine (remake of the 1996 RTS 'Z')"
   homepage "https://github.com/fenio/zod"
-  url "https://github.com/fenio/zod/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "d28f4f487eb13a874d4c92e43a52781afd45bd5fc3b69ed399e06a6704ed7227"
+  url "https://github.com/fenio/zod/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "5cc560650d5e724af340cae30ffbbd8c3e230f1602302e6cd3be1f4888b69c53"
   license "GPL-3.0-or-later"
 
-  bottle do
-    root_url "https://github.com/fenio/homebrew-tap/releases/download/bottle-zod-0.2.2"
-    sha256 arm64_sonoma: "d3feb00f50a49730ab5c6a0a30a4a91f4d5e5578a7132fabead27c1d1cf8a506"
-  end
 
 
 
