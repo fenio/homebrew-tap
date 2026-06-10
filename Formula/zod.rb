5734f43d0212a2c1eaec5937b8bab69083685e67
fenio <bartosz@fenski.pl>
2026-06-11 00:26:54 +0200

    zod 0.3.0: native SDL3 (sdl2->sdl3 deps, new url/sha; bottle rebuilt by workflow)

--- Formula/zod.rb
+++ Formula/zod.rb
@@ -1,25 +1,16 @@
 class Zod < Formula
-  desc "Modernized SDL2 build of the Zod Engine (remake of the 1996 RTS 'Z')"
+  desc "Modernized SDL3 build of the Zod Engine (remake of the 1996 RTS 'Z')"
   homepage "https://github.com/fenio/zod"
-  url "https://github.com/fenio/zod/archive/refs/tags/v0.2.3.tar.gz"
-  sha256 "5cc560650d5e724af340cae30ffbbd8c3e230f1602302e6cd3be1f4888b69c53"
+  url "https://github.com/fenio/zod/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "179c37dd14fe41f70c28144dbb4250ab30d0b46edaa187c4b43689d822109df9"
   license "GPL-3.0-or-later"
-
-  bottle do
-    root_url "https://github.com/fenio/homebrew-tap/releases/download/bottle-zod-0.2.3"
-    sha256 arm64_sonoma: "cb5bb4e86f5adce13d0c2a02cb54df7f5a404f78bb69ff5fa0cdaf5514385d4e"
-  end
-
-
-
-
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "sdl2"
-  depends_on "sdl2_image"
-  depends_on "sdl2_mixer"
-  depends_on "sdl2_ttf"
+  depends_on "sdl3"
+  depends_on "sdl3_image"
+  depends_on "sdl3_mixer"
+  depends_on "sdl3_ttf"
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
