26a68ea2a80bbd12893bca3a10c5410754a0619e
Bartosz Fenski <fenio@debian.org>
2026-06-12 08:58:42 +0200

    zod 0.3.2: bump url/sha (bottle rebuilt by workflow)

--- Formula/zod.rb
+++ Formula/zod.rb
@@ -1,14 +1,9 @@
 class Zod < Formula
   desc "Modernized SDL3 build of the Zod Engine (remake of the 1996 RTS 'Z')"
   homepage "https://github.com/fenio/zod"
-  url "https://github.com/fenio/zod/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "682c9f95779d050e64aaa72794d044b9ce45a6c3f86f3da290c54ae405d7ba34"
+  url "https://github.com/fenio/zod/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "1c1596e6dcb0c1d979ff5e769245dc883b931e23362772d7a28c3d4a45308e89"
   license "GPL-3.0-or-later"
-
-  bottle do
-    root_url "https://github.com/fenio/homebrew-tap/releases/download/bottle-zod-0.3.1"
-    sha256 arm64_sonoma: "caaa410ded00772162b10fc04e88684144a8a6ebc9a3dae3b6ff4af3535c3d2c"
-  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
