bb1c046ec4b458095572beab7278bfbb8e7d9396
Bartosz Fenski <fenio@debian.org>
2026-06-16 00:45:50 +0200

    zod 0.4.4: bump url/sha, drop stale bottle block

--- Formula/zod.rb
+++ Formula/zod.rb
@@ -1,25 +1,9 @@
 class Zod < Formula
   desc "Modernized SDL3 build of the Zod Engine (remake of the 1996 RTS 'Z')"
   homepage "https://github.com/fenio/zod"
-  url "https://github.com/fenio/zod/archive/refs/tags/v0.4.3.tar.gz"
-  sha256 "e9809fae94d6e5f567f61c53825b343bc740db831d2c4727b2da1cbaed98a5c3"
+  url "https://github.com/fenio/zod/archive/refs/tags/v0.4.4.tar.gz"
+  sha256 "ce2a3a75fd3942f400dc5a38ffa3485524e6d32655b95c995586d5ac4932618a"
   license "GPL-3.0-or-later"
-
-  bottle do
-    root_url "https://github.com/fenio/homebrew-tap/releases/download/bottle-zod-0.4.3"
-    sha256 arm64_sonoma: "67ae61c1e53f479ebc570552c8c6be6ef8a15f36b3fd047ef98ba4baed181e0d"
-  end
-
-  bottle do
-    root_url "https://github.com/fenio/homebrew-tap/releases/download/bottle-zod-0.4.3"
-    sha256 arm64_sonoma: "67ae61c1e53f479ebc570552c8c6be6ef8a15f36b3fd047ef98ba4baed181e0d"
-  end
-
-
-
-
-
-
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
